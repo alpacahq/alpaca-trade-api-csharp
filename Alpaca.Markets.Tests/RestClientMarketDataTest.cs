7070760b5561a6e373a582eb80d042d3766ecbfa
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2018-06-19 11:13:03 +0200

    Bar data requesting/processing logic implemented and covered by tests.

--- Alpaca.Markets.Tests/RestClientMarketDataTest.cs
+++ Alpaca.Markets.Tests/RestClientMarketDataTest.cs
@@ -1,10 +1,68 @@
-﻿using Xunit;
+﻿using System;
+using System.Linq;
+using Xunit;
 
 namespace Alpaca.Markets.Tests
 {
     public sealed class RestClientMarketDataTest
     {
         private readonly RestClient _restClient = RestClientFactory.GetRestClient();
+
+        [Theory]
+        [InlineData(BarDuration.OneMinute, 4)]
+        [InlineData(BarDuration.FiveMinutes, 5)]
+        [InlineData(BarDuration.FifteenMinutes, 7)]
+        [InlineData(BarDuration.OneHour, 7)]
+        [InlineData(BarDuration.OneDay, 14)]
+        public async void GetMultiAssetBarsWorks(
+            BarDuration barDuration,
+            Int32 daysLookback)
+        {
+            var endTimeInclusive = DateTime.UtcNow;
+            var startTimeInclusive = endTimeInclusive
+                .Subtract(TimeSpan.FromDays(daysLookback));
+
+            var collection = await _restClient.GetBarsAsync(
+                new [] { "AAPL", "GOOG", "MSFT" },
+                barDuration, startTimeInclusive, endTimeInclusive);
+
+            Assert.NotNull(collection);
+
+            var list = collection.ToList();
+
+            Assert.NotEmpty(list);
+            Assert.Equal(3, list.Count);
+        }
+
+        [Theory]
+        [InlineData(BarDuration.OneMinute, 4)]
+        [InlineData(BarDuration.FiveMinutes, 5)]
+        [InlineData(BarDuration.FifteenMinutes, 7)]
+        [InlineData(BarDuration.OneHour, 7)]
+        [InlineData(BarDuration.OneDay, 14)]
+        public async void GetSingleAssetBarsWorks(
+            BarDuration barDuration,
+            Int32 daysLookback)
+        {
+            var endTimeInclusive = DateTime.UtcNow;
+            var startTimeInclusive = endTimeInclusive
+                .Subtract(TimeSpan.FromDays(daysLookback));
+
+            var bars = await _restClient.GetBarsAsync(
+                "AAPL", barDuration,
+                startTimeInclusive, endTimeInclusive);
+
+            Assert.NotNull(bars);
+            Assert.Equal("AAPL", bars.Symbol);
+
+            Assert.NotNull(bars.Items);
+            Assert.NotEmpty(bars.Items);
+
+            var first = bars.Items.First();
+            var last = bars.Items.Last();
+
+            Assert.True(first.Time < last.Time);
+        }
 
         [Fact]
         public async void GetQuotesWorks()
---
+++ Alpaca.Markets/Interfaces/IBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Alpaca.Markets
+{
+    public interface IBar
+    {
+        Decimal Open { get; }
+
+        Decimal High { get; }
+
+        Decimal Low { get; }
+
+        Decimal Close { get; }
+
+        Int64 Volume { get; }
+
+        DateTime Time { get; }
+    }
+}
