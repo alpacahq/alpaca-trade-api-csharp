315ffde6d0b8208d0b51895d751f8ea31a81da09
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2018-08-21 15:52:35 +0200

    Issue #12 - incorrect handling of 'null' value for ListHistoricalQuotesAsync method. If returned JSON contains 'null' value in 'ticks' field we have to handle it w/o exception and return empty list.

---
+++ Alpaca.Markets.Tests/RestClientExtendedTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace Alpaca.Markets.Tests
+{
+    public sealed class RestClientExtendedTest
+    {
+        private readonly RestClient _restClient = ClientsFactory.GetRestClient();
+
+        [Fact]
+        public async void ListHistoricalQuotesReturnsEmptyListForSunday()
+        {
+            var historicalItems = await _restClient
+                .ListHistoricalQuotesAsync("AAPL", new DateTime(2018, 8, 5));
+
+            Assert.NotNull(historicalItems);
+
+            Assert.NotNull(historicalItems.Items);
+            Assert.Empty(historicalItems.Items);
+        }
+    }
+}
