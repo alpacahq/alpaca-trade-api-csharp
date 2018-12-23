84dadc351c9094d1e37e24c60c4752e7e8b91d8a
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2018-12-23 09:30:50 +0100

    Issue #39 fixed
    We should use different time zone names for  EST time zone under Windows and Linux/MacOS.

--- Alpaca.Markets/Helpers/CustomTimeZone.cs
+++ Alpaca.Markets/Helpers/CustomTimeZone.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Alpaca.Markets
 {
     internal static class CustomTimeZone
     {
+#if NETFRAMEWORK
         public static TimeZoneInfo Est { get; } =
             TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+#else
+        public static TimeZoneInfo Est { get; } =
+            TimeZoneInfo.FindSystemTimeZoneById(
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                    ? "Eastern Standard Time"
+                    : "America/New_York");
+#endif
     }
 }
