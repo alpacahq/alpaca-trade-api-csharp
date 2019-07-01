6945c4769a2169a421aa408cdf2612b2d5bb41e7
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2019-07-01 21:30:00 +0200

    Issue #27 - make WebSocket client pluggable
    Added two new interfaces - `IWebScoketFactory` and `IWebSocket`
    SockClient uses only these interfaces for communicating through WebSockets with server
    Default implementation based on WebSocket4Net library added into library

---
+++ Alpaca.Markets/WebSocket/IWebSocketFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Alpaca.Markets
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IWebSocketFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        IWebSocket CreateWebSocket(Uri url);
+    }
+}
---
+++ Alpaca.Markets/WebSocket/WebSocket4NetFactory.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using SuperSocket.ClientEngine;
+using WebSocket4Net;
+
+namespace Alpaca.Markets
+{
+    internal sealed class WebSocket4NetFactory : IWebSocketFactory
+    {
+        private sealed class WebSocketWrapper : IWebSocket
+        {
+            private readonly WebSocket _webSocket;
+
+            public WebSocketWrapper(Uri url)
+            {
+                _webSocket = new WebSocket(url.ToString(),
+                    sslProtocols: SslProtocols.Tls11 | SslProtocols.Tls12);
+
+                _webSocket.Opened += onWebSocketOnOpened;
+                _webSocket.Closed += onWebSocketOnClosed;
+
+                _webSocket.DataReceived += onWebSocketOnDataReceived;
+                _webSocket.Error += onWebSocketOnError;
+            }
+
+            public void Dispose()
+            {
+                if (_webSocket == null)
+                {
+                    return;
+                }
+
+                _webSocket.Opened -= onWebSocketOnOpened;
+                _webSocket.Closed -= onWebSocketOnClosed;
+
+                _webSocket.DataReceived -= onWebSocketOnDataReceived;
+                _webSocket.Error -= onWebSocketOnError;
+
+                _webSocket?.Dispose();
+            }
+
+            public Task OpenAsync()
+            {
+#if NET45
+            return Task.Run(() => _webSocket.Open());
+#else
+                return _webSocket.OpenAsync();
+#endif
+            }
+
+            public Task CloseAsync()
+            {
+#if NET45
+            return Task.Run(() => _webSocket.Close());
+#else
+                return _webSocket.CloseAsync();
+#endif
+            }
+
+            public void Send(String message)
+            {
+                _webSocket.Send(message);
+            }
+
+            public event Action Opened;
+
+            public event Action Closed;
+
+            public event Action<Byte[]> DataReceived;
+
+            public event Action<Exception> Error;
+
+            private void onWebSocketOnClosed(Object sender, EventArgs eventArgs)
+            {
+                Closed?.Invoke();
+            }
+
+            private void onWebSocketOnOpened(Object sender, EventArgs eventArgs)
+            {
+                Opened?.Invoke();
+            }
+
+            private void onWebSocketOnError(Object sender, ErrorEventArgs eventArgs)
+            {
+                Error?.Invoke(eventArgs.Exception);
+            }
+
+            private void onWebSocketOnDataReceived(Object sender, DataReceivedEventArgs eventArgs)
+            {
+                DataReceived?.Invoke(eventArgs.Data);
+            }
+        }
+
+        public IWebSocket CreateWebSocket(Uri url)
+        {
+            return new WebSocketWrapper(url);
+        }
+    }
+}
