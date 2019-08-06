492b35521154f351548950db8120ee55ea5ef636
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2019-08-06 21:54:30 +0200

    Issue #140 - Polygon.io web socket authentication
    * Authentication request should be sent only after receiving request from server side.
    * All possible answer with connection statuses should be processed correctly.
    * Constructor of `PolygonSockClient' should receive 'isStagingEnvironment` parameter.
    
    (cherry picked from commit 3aeb45b2f7a8a429abe84c93844dc30d81890ea6)

---
+++ Alpaca.Markets/Enums/ConnectionStatus.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Alpaca.Markets
+{
+    /// <summary>
+    /// Authorization status for Alpaca streaming API.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ConnectionStatus
+    {
+        /// <summary>
+        /// Client successfully connected.
+        /// </summary>
+        [EnumMember(Value = "connected")]
+        Connected,
+
+        /// <summary>
+        /// Client successfully authorized.
+        /// </summary>
+        [EnumMember(Value = "auth_success")]
+        Success,
+
+        /// <summary>
+        /// Client authentication required.
+        /// </summary>
+        [EnumMember(Value = "auth_required")]
+        AuthenticationRequired,
+
+        /// <summary>
+        /// Client authentication failed.
+        /// </summary>
+        [EnumMember(Value = "auth_failed")]
+        AuthenticationFailed
+    }
+}
---
+++ Alpaca.Markets/Messages/JsonConnectionStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Alpaca.Markets
+{
+    internal sealed class JsonConnectionStatus
+    {
+        [JsonProperty(PropertyName = "status", Required = Required.Always)]
+        public ConnectionStatus Status { get; set; }
+
+        [JsonProperty(PropertyName = "message", Required = Required.Always)]
+        public String Message { get; set; }
+    }
+}
