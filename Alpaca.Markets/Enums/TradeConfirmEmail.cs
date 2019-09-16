09e2acb04f28cb9648b911b6f7523b5db55e9f9d
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2019-09-16 23:08:05 +0200

    Issue #130 - implement account configuration support
    Add new mutable interface `IAccountCofiguration` and corresponding class for
    proper JSON (de) serialization of this interface into SDK. Add two new methods
    `GetAccountConfigurationAsync` and `PatchAccountConfigurationAsync` into
    `RestClient` class for reading and updating account configuration settings.

---
+++ Alpaca.Markets/Enums/TradeConfirmEmail.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Alpaca.Markets
+{
+    /// <summary>
+    /// Notification level for order fill emails.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TradeConfirmEmail
+    {
+        /// <summary>
+        /// Never send email notification for order fills.
+        /// </summary>
+        [EnumMember(Value = "none")]
+        None,
+
+        /// <summary>
+        /// Send email notification for all order fills.
+        /// </summary>
+        [EnumMember(Value = "all")]
+        All
+    }
+}
