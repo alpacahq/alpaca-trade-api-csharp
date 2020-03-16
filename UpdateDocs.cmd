6dc4a363e8871de1e85109730f5d0a9460ecda52
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-03-16 08:57:27 +0100

    We shouldn't reset HEAD after subtree push - it makes subtree unusable.

--- UpdateDocs.cmd
+++ UpdateDocs.cmd
@@ -1,5 +1,4 @@
 @echo off
 git add Documentation/_site/** -f
 git commit -m "Update content of .NET SDK documentation on GitHub Pages web site."
-git subtree push --prefix Documentation/_site https://github.com/alpacahq/alpaca-trade-api-csharp.git gh-pages
-git reset --hard HEAD^
+git subtree push --prefix Documentation/_site origin gh-pages
