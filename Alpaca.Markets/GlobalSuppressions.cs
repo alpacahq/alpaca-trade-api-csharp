28326d126fdc452f85b3d5033bec595a74a30030
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-04-07 12:23:02 +0200

    Update content of .NET SDK documentation on GitHub Pages web site.

--- Alpaca.Markets/GlobalSuppressions.cs
+++
@@ -1,5 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
-    "Globalization",    "CA1303:Do not pass literals as localized parameters",
-    Justification = "We do not plan to support localized exception messages in this SDK.")]
--- Alpaca.Markets/Helpers/NullableHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Alpaca.Markets
-{
-    internal static class NullableHelper
-    {
-        public static T EnsureNotNull<T>(this T value, String name) where T : class => value ?? throw new ArgumentNullException(name);
-    }
-}
