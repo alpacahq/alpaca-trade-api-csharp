a7696c8f700e5a6d55061c167d739f7a1ec48dcf
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-04-24 15:40:59 +0200

    Issue #201 - The new property `RollUpNestedOrder` added into the `ListOrdersRequest` class for supporting nested orders roll-up on the server-side.
    (cherry picked from commit 02730a19eb5ab2975ee05cdfce8b2f21b183d6af)

--- Alpaca.Markets/AlpacaTradingClient.Orders.cs
+++ Alpaca.Markets/AlpacaTradingClient.Orders.cs
@@ -39,6 +39,7 @@ namespace Alpaca.Markets
                     .AddParameter("until", request.UntilDateTimeExclusive, "O")
                     .AddParameter("after", request.AfterDateTimeExclusive, "O")
                     .AddParameter("limit", request.LimitOrderNumber)
+                    .AddParameter("nested", request.RollUpNestedOrder)
             };
 
             return _httpClient.GetObjectsListAsync<IOrder, JsonOrder>(
