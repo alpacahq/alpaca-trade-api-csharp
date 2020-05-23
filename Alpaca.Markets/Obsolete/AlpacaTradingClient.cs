201b67480f28822c484ff4b4c1460e8dd8fd432b
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-05-23 13:03:50 +0200

    Issue #209 - Add new method overload (with request parameter) that returns the `IOrder` object.

--- Alpaca.Markets/Obsolete/AlpacaTradingClient.cs
+++ Alpaca.Markets/Obsolete/AlpacaTradingClient.cs
@@ -355,5 +355,27 @@ namespace Alpaca.Markets
                 new ChangeWatchListRequest<string>(
                     name, asset),
                 cancellationToken);
+
+        
+        /// <summary>
+        /// Liquidate an open position at market price using Alpaca REST API endpoint.
+        /// </summary>
+        /// <param name="symbol">Symbol for liquidation.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns><c>True</c> if position liquidation was accepted.</returns>
+        [Obsolete("Use overloaded method that required DeletePositionRequest parameter instead of this one.", false)]
+        public async Task<Boolean> DeletePositionAsync(
+            String symbol,
+            CancellationToken cancellationToken = default)
+        {
+            await _alpacaRestApiThrottler.WaitToProceed(cancellationToken).ConfigureAwait(false);
+
+            using var response = await _httpClient.DeleteAsync(
+                    new Uri($"positions/{symbol}", UriKind.RelativeOrAbsolute), cancellationToken)
+                .ConfigureAwait(false);
+
+            return response.IsSuccessStatusCode;
+        }
+
     }
 }
