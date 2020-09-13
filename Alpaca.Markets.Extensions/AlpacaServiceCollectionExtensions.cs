e3cdb66ff5557dfc4c01c3bca4e02e0eb7100184
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-09-13 13:05:43 +0200

    Issue #207 - added additional NuGet package with helper methods for registering strongly-typed HTTP REST clients in the default DI container.

---
+++ Alpaca.Markets.Extensions/AlpacaServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alpaca.Markets.Extensions
+{
+    public static class AlpacaServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAlpacaDataClient(
+            this IServiceCollection services,
+            IEnvironment environment,
+            SecurityKey securityKey) =>
+            services
+                .AddHttpClient<IAlpacaDataClient>()
+                .AddTypedClient<IAlpacaDataClient>(
+                    httpClient => new AlpacaDataClient(
+                        environment
+                            .GetAlpacaDataClientConfiguration(securityKey)
+                            .withFactoryCreatedHttpClient(httpClient)))
+                .Services;
+
+        public static IServiceCollection AddAlpacaTradingClient(
+            this IServiceCollection services,
+            IEnvironment environment,
+            SecurityKey securityKey) =>
+            services
+                .AddHttpClient<IAlpacaTradingClient>()
+                .AddTypedClient<IAlpacaTradingClient>(
+                    httpClient => new AlpacaTradingClient(
+                        environment
+                            .GetAlpacaTradingClientConfiguration(securityKey)
+                            .withFactoryCreatedHttpClient(httpClient)))
+                .Services;
+
+        private static AlpacaDataClientConfiguration withFactoryCreatedHttpClient(
+            this AlpacaDataClientConfiguration configuration,
+            HttpClient httpClient)
+        {
+            configuration.HttpClient = httpClient;
+            return configuration;
+        }
+
+        private static AlpacaTradingClientConfiguration withFactoryCreatedHttpClient(
+            this AlpacaTradingClientConfiguration configuration,
+            HttpClient httpClient)
+        {
+            configuration.HttpClient = httpClient;
+            return configuration;
+        }
+    }
+}
---
+++ Alpaca.Markets.Extensions/PolygonServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alpaca.Markets.Extensions
+{
+    public static class PolygonServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPolygonDataClient(
+            this IServiceCollection services,
+            IEnvironment environment,
+            String keyId) =>
+            services
+                .AddHttpClient<IPolygonDataClient>()
+                .AddTypedClient<IPolygonDataClient>(
+                    httpClient => new PolygonDataClient(
+                        environment
+                            .GetPolygonDataClientConfiguration(keyId)
+                            .withFactoryCreatedHttpClient(httpClient)))
+                .Services;
+
+        private static PolygonDataClientConfiguration withFactoryCreatedHttpClient(
+            this PolygonDataClientConfiguration configuration,
+            HttpClient httpClient)
+        {
+            configuration.HttpClient = httpClient;
+            return configuration;
+        }
+    }
+}
