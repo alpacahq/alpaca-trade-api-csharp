66d97ecc19af9108fd182e98085c62a3517e966d
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-10-26 12:36:35 +0100

    Issue #249 - the reconnection wrapper for the `IAlpacaStreamingClient` interface implemented.

---
+++ Alpaca.Markets.Extensions/Reconnection/AlpacaStreamingClientExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Alpaca.Markets.Extensions
+{
+    /// <summary>
+    /// Helper extension method for creating special version of the <see cref="IAlpacaStreamingClient"/>
+    /// implementation with automatic reconnection (with configurable delay and number of attempts) support.
+    /// </summary>
+    public static class AlpacaStreamingClientExtensions
+    {
+        private sealed class ClientWithReconnection :
+            ClientWithReconnectBase<IAlpacaStreamingClient, Int32>,
+            IAlpacaStreamingClient
+        {
+            public ClientWithReconnection(
+                IAlpacaStreamingClient client,
+                ReconnectionParameters reconnectionParameters)
+                : base (client, reconnectionParameters)
+            {
+            }
+
+            public event Action<IAccountUpdate>? OnAccountUpdate
+            {
+                add => Client.OnAccountUpdate += value;
+                remove => Client.OnAccountUpdate += value;
+            }
+
+            public event Action<ITradeUpdate>? OnTradeUpdate
+            {
+                add => Client.OnTradeUpdate += value;
+                remove => Client.OnTradeUpdate += value;
+            }
+
+            protected override void Resubscribe(String symbol, Int32 subscription) { }
+        }
+
+        /// <summary>
+        /// Wraps instance of <see cref="IAlpacaStreamingClient"/> into the helper class
+        /// with automatic reconnection support and provide optional reconnection parameters.
+        /// </summary>
+        /// <param name="client">Original streaming client for wrapping.</param>
+        /// <param name="parameters">Reconnection parameters (or default if missing).</param>
+        /// <returns>Wrapped version of the <paramref name="client"/> object with reconnect.</returns>
+        public static IAlpacaStreamingClient WithReconnect(
+            this IAlpacaStreamingClient client,
+            ReconnectionParameters? parameters = null) =>
+            new ClientWithReconnection(client, parameters ?? ReconnectionParameters.Default);
+    }
+}
