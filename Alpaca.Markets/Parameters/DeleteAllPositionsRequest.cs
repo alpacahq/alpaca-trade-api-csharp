cc30c79e2cae794190484354af73414b96e5c5ef
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-12-18 14:45:55 +0100

    Issue #267 - Added the new `DeleteAllPositionsAsync` overload into the `IAlpacaTradingClient` interface and implementation - provides support for canceling all open orders alongside the all positions liquidation.
    (cherry picked from commit 7793308ca55591c1128761a49f4b0bcc8316bb01)
    (cherry picked from commit 58f1c8d1d4a2716bb5d3ed28009c4e0d2bcde12c)

---
+++ Alpaca.Markets/Parameters/DeleteAllPositionsRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using JetBrains.Annotations;
+
+namespace Alpaca.Markets
+{
+    /// <summary>
+    /// Encapsulates request parameters for <see cref="AlpacaTradingClient.DeleteAllPositionsAsync(DeleteAllPositionsRequest,System.Threading.CancellationToken)"/> call.
+    /// </summary>
+    public sealed class DeleteAllPositionsRequest
+    {
+        /// <summary>
+        /// Gets the flag indicating that request should also cancel all open orders (false if null).
+        /// </summary>
+        [UsedImplicitly]
+        public Boolean? CancelOrders { get; set; }
+
+        internal UriBuilder GetUriBuilder(
+            HttpClient httpClient) =>
+            new UriBuilder(httpClient.BaseAddress)
+            {
+                Path = $"v2/positions",
+                Query = new QueryBuilder()
+                    .AddParameter("cancel_orders", CancelOrders)
+            };
+    }
+}
