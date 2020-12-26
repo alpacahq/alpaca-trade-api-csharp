bacfc0c876ecf894b73d43720630a932cd49eaf1
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2020-12-26 20:44:42 +0100

    Issue #254 - separate workflow (manual) for building/publishing documentation.

--- UpdateDocs.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-git add Documentation/_site/** -f
-git commit -m "Update content of .NET SDK documentation on GitHub Pages web site."
-git subtree push --prefix Documentation/_site origin gh-pages
