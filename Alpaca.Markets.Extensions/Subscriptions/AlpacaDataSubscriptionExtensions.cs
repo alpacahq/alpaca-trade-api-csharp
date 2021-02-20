cc01a609a7b41a7656b58f5ca2156f7a37e72d3f
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-02-20 15:48:32 +0100

    Issue #144 - Added the extension method `AsAsyncEnumerable` for converting the `IAlpacaDataSubscription<TItem>.Received` event into the instance of the `IAsyncEnumerable<TItem>` interface.

---
+++ Alpaca.Markets.Extensions/Subscriptions/AlpacaDataSubscriptionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Alpaca.Markets.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class AlpacaDataSubscriptionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subscription"></param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="TItem"></typeparam>
+        /// <returns></returns>
+        public static async IAsyncEnumerable<TItem> AsAsyncEnumerable<TItem>(
+            this IAlpacaDataSubscription<TItem> subscription,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+            where TItem : IStreamBase
+        {
+            subscription.EnsureNotNull(nameof(subscription));
+
+            var buffer = Channel.CreateUnbounded<TItem>();
+            subscription.Received += HandleReceived;
+
+            try
+            {
+                while (await buffer.Reader
+                    .WaitToReadAsync(cancellationToken)
+                    .ConfigureAwait(false))
+                {
+                    yield return await buffer.Reader
+                        .ReadAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                }
+            }
+            finally
+            {
+                subscription.Received -= HandleReceived;
+                buffer.Writer.TryComplete();
+            }
+
+            void HandleReceived(TItem item) => buffer.Writer.TryWrite(item);
+        }
+    }
+}
