f5d40c6fdaf75a7af5aa05d572dcfc9300a251f3
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-03-16 12:23:09 +0100

    Merge both `GlobalSuppressions.cs` files and move it (as well as some other shared files) into the solution-level folder.

--- Alpaca.Markets.Extensions/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
-
-using System;
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: CLSCompliant(false)]
-[assembly: SuppressMessage("Globalization",    
-    "CA1303: Do not pass literals as localized parameters",
-    Justification = "We do not plan to support localized exception messages in this SDK.")]
