e246664f917bb7220dbfa13cbd45ec32adf64a50
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-03-26 11:44:21 +0100

    Issue #305 - added meaningful XML comments and some attributes for R# code checks.

--- Alpaca.Markets/Enums/Multiplier.cs
+++ Alpaca.Markets/Enums/Multiplier.cs
@@ -4,30 +4,32 @@ using System.Diagnostics.CodeAnalysis;
 namespace Alpaca.Markets
 {
     /// <summary>
-    /// 
+    /// Account multiplier value (enum values can be cast to related integer values).
     /// </summary>
     [Flags]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     [SuppressMessage("Naming", "CA1720:Identifier contains type name",
         Justification = "We use these names because they explain situation better.")]
     public enum Multiplier
     {
         /// <summary>
-        /// 
+        /// Invalid default value of enum.
         /// </summary>
         None = 0,
 
         /// <summary>
-        /// 
+        /// Standard limited margin account with 1x buying power.
         /// </summary>
         Single = 1,
 
         /// <summary>
-        /// 
+        /// Regular T margin account with 2x intraday and overnight buying power;
+        /// this is the default for all non-PDT accounts with $2,000 or more equity.
         /// </summary>
         Double = 2,
 
         /// <summary>
-        /// 
+        /// PDT account with 4x intraday buying power and 2x reg T overnight buying power.
         /// </summary>
         Quadruple = 4
     }
