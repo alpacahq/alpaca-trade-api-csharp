b46a59f9d1d8999008d35d49c837069416a43207
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-09-27 20:05:59 +0200

    Issue #357 - added new metadata requesting methods into the `IAlpacaDataClient` interface.
    (cherry picked from commit a8824b39ae5a8f105305734bc0c2a305f1273d1c)

---
+++ Alpaca.Markets/Enums/Tape.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Alpaca.Markets
+{
+    /// <summary>
+    /// Supported tape types for Alpaca Data API.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Tape
+    {
+        /// <summary>
+        /// Tape A - NYSE.
+        /// </summary>
+        [UsedImplicitly]
+        A,
+
+        /// <summary>
+        /// Tape B - NYSE Arca and NYSE Amex.
+        /// </summary>
+        [UsedImplicitly]
+        B,
+
+        /// <summary>
+        /// Tape C - NAsDAQ.
+        /// </summary>
+        [UsedImplicitly]
+        C
+    }
+}
