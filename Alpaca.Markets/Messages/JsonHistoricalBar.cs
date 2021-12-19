be4c89ba8cbf5c6ac3f7dc65761334486d93a483
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-12-19 13:36:26 +0100

    Issue #432 - Fixed problem with the `IBar.Vwap` and `IBar.TradeCount` properties deserialization.
    (cherry picked from commit bbb030b9f2d7fa7fccdabb3a4c8dbb7525983c7b)

--- Alpaca.Markets/Messages/JsonHistoricalBar.cs
+++ Alpaca.Markets/Messages/JsonHistoricalBar.cs
@@ -31,9 +31,9 @@ namespace Alpaca.Markets
         public DateTime TimeUtc { get; set; }
 
         [JsonProperty(PropertyName = "vw", Required = Required.Default)]
-        public Decimal Vwap { get; }
+        public Decimal Vwap { get; set; }
 
         [JsonProperty(PropertyName = "n", Required = Required.Default)]
-        public UInt64 TradeCount { get; }
+        public UInt64 TradeCount { get; set; }
     }
 }
