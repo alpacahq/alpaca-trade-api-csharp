2636145c41478864734861a69296609017058b06
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2021-12-25 08:36:04 +0100

    Issue #435 - Added extensions method for requesting all account activities in pagination mode.
    (cherry picked from commit 8d5ad460c79a24fd946c5f47869264e9ae03d703)
    (cherry picked from commit 9ff18acf1aa00952df99a7df3340022c600b17d1)

---
+++ Alpaca.Markets.Extensions/Pagination/AlpacaTradingClientExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using JetBrains.Annotations;
+
+namespace Alpaca.Markets.Extensions
+{
+    /// <summary>
+    /// Set of extension methods for the <see cref="IAlpacaTradingClient"/> interface.
+    /// </summary>
+    public static class AlpacaTradingClientExtensions
+    {
+        /// <summary>
+        /// Gets all account activities from Alpaca REST API endpoint as async enumerable stream.
+        /// </summary>
+        /// <param name="client">The <see cref="IAlpacaDataClient"/> object instance.</param>
+        /// <param name="request">Account activities request parameters.</param>
+        /// <returns>Account activity record objects obtained page by page.</returns>
+        [UsedImplicitly]
+        [CLSCompliant(false)]
+        public static IAsyncEnumerable<IAccountActivity> ListAccountActivitiesAsAsyncEnumerable(
+            this IAlpacaTradingClient client,
+            AccountActivitiesRequest request) =>
+            ListAccountActivitiesAsAsyncEnumerable(client, request, CancellationToken.None);
+
+        /// <summary>
+        /// Gets all account activities from Alpaca REST API endpoint as async enumerable stream.
+        /// </summary>
+        /// <param name="client">The <see cref="IAlpacaDataClient"/> object instance.</param>
+        /// <param name="request">Account activities request parameters.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Account activity record objects obtained page by page.</returns>
+        [UsedImplicitly]
+        [CLSCompliant(false)]
+        public static IAsyncEnumerable<IAccountActivity> ListAccountActivitiesAsAsyncEnumerable(
+            this IAlpacaTradingClient client,
+            AccountActivitiesRequest request,
+            CancellationToken cancellationToken) =>
+            getAllAccountActivitiesPages(client.EnsureNotNull(nameof(client)),
+                getRequestWithoutPageToken(request.EnsureNotNull(nameof(request))), cancellationToken);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static AccountActivitiesRequest getRequestWithoutPageToken(
+            AccountActivitiesRequest request) =>
+            new (request.ActivityTypes)
+            {
+                Direction = request.Direction
+            };
+
+        private static async IAsyncEnumerable<IAccountActivity> getAllAccountActivitiesPages(
+            IAlpacaTradingClient client,
+            AccountActivitiesRequest request,
+            [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            do
+            {
+                var activities = await client
+                    .ListAccountActivitiesAsync(request, cancellationToken).ConfigureAwait(false);
+                if (activities.Count == 0)
+                {
+                    break;
+                }
+
+                foreach (var item in activities)
+                {
+                    yield return item;
+                }
+
+                request.PageToken = activities[activities.Count - 1].ActivityId;
+            } while (!String.IsNullOrEmpty(request.PageToken));
+        }
+    }
+}
