014266f0adf6f955f010ce05ce52dc7eead7ee67
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-03-21 11:08:12 +0100

    Issue #481 - Fixed problem with fractional order quantity validation for market and bracket orders.
    (cherry picked from commit e4423f81d04c1096a80f022076890933ad32a576)

---
+++ Alpaca.Markets.Extensions/Orders/OrderSideExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Alpaca.Markets.Extensions
+{
+
+    /// <summary>
+    /// Set of extensions methods for creating the <see cref="OrderBase"/> inheritors.
+    /// </summary>
+    public static class OrderSideExtensions
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="BracketOrder"/> order using
+        /// specified side, symbol, quantity, take profit, and stop loss prices.
+        /// </summary>
+        /// <param name="orderSide">Order side (buy or sell).</param>
+        /// <param name="symbol">Order asset name.</param>
+        /// <param name="quantity">Order quantity.</param>
+        /// <param name="takeProfitLimitPrice">Take profit order limit price.</param>
+        /// <param name="stopLossStopPrice">Stop loss order stop price.</param>
+        /// <returns>
+        /// New advanced order representing an original order plus pair of take profit and stop loss orders.
+        /// </returns>
+        [UsedImplicitly]
+        public static BracketOrder Bracket(
+            this OrderSide orderSide,
+            String symbol,
+            OrderQuantity quantity,
+            Decimal takeProfitLimitPrice,
+            Decimal stopLossStopPrice) =>
+            orderSide.Market(symbol, quantity)
+                .Bracket(takeProfitLimitPrice, stopLossStopPrice);
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="BracketOrder"/> order using
+        /// specified side, symbol, quantity, take profit, and stop loss prices.
+        /// </summary>
+        /// <param name="orderSide">Order side (buy or sell).</param>
+        /// <param name="symbol">Order asset name.</param>
+        /// <param name="quantity">Order quantity.</param>
+        /// <param name="takeProfitLimitPrice">Take profit order limit price.</param>
+        /// <param name="stopLossStopPrice">Stop loss order stop price.</param>
+        /// <param name="stopLossLimitPrice">Stop loss order limit price.</param>
+        /// <returns>
+        /// New advanced order representing an original order plus pair of take profit and stop loss orders.
+        /// </returns>
+        [UsedImplicitly]
+        public static BracketOrder Bracket(
+            this OrderSide orderSide,
+            String symbol,
+            OrderQuantity quantity,
+            Decimal takeProfitLimitPrice,
+            Decimal stopLossStopPrice,
+            Decimal stopLossLimitPrice) =>
+            orderSide.Market(symbol, quantity)
+                .Bracket(takeProfitLimitPrice, stopLossStopPrice, stopLossLimitPrice);
+    }
+}
