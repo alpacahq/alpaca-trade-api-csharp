a4e727070f42d87d4bfef5b5c77b80658d0ce897
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-04-21 17:01:45 +0200

    Issue #500 - Added the new `HttpStatusCode` property into the `RestClientErrorException` class with value of the original HTTP return status code.

--- Alpaca.Markets/Exceptions/RestClientErrorException.cs
+++ Alpaca.Markets/Exceptions/RestClientErrorException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Net;
 using System.Net.Http;
 
 namespace Alpaca.Markets
@@ -47,9 +48,13 @@ namespace Alpaca.Markets
         }
 
         internal RestClientErrorException(
+            HttpResponseMessage message,
             JsonError error)
-            : base(error.Message) =>
+            : base(error.Message)
+        {
+            HttpStatusCode = message.StatusCode;
             ErrorCode = error.Code;
+        }
 
         internal RestClientErrorException(
             JsonStreamError error)
@@ -59,17 +64,22 @@ namespace Alpaca.Markets
         internal RestClientErrorException(
             HttpResponseMessage message)
             : base(message.ReasonPhrase ?? String.Empty) =>
-            ErrorCode = (Int32)message.StatusCode;
+            ErrorCode = (Int32)(HttpStatusCode = message.StatusCode);
 
         internal RestClientErrorException(
             HttpResponseMessage message,
             Exception exception)
             : base(message.ReasonPhrase ?? String.Empty, exception) =>
-            ErrorCode = (Int32)message.StatusCode;
+            ErrorCode = (Int32)(HttpStatusCode = message.StatusCode);
 
         /// <summary>
-        /// Original error code returned by REST API endpoint.
+        /// Gets original error code returned by REST API endpoint.
         /// </summary>
         public Int32 ErrorCode { get; }
+
+        /// <summary>
+        /// Gets original HTTP status code returned by REST API endpoint.
+        /// </summary>
+        public HttpStatusCode? HttpStatusCode { get; }
     }
 }
