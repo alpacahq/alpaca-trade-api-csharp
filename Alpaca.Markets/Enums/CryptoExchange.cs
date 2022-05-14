5d5f4841eb7280a11d4c3989de2415469fe9687e
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-05-14 10:44:18 +0200

    Removed obsolete item `Gnss` from the `CryptoExchange` enum.

--- Alpaca.Markets/Enums/CryptoExchange.cs
+++ Alpaca.Markets/Enums/CryptoExchange.cs
@@ -35,12 +35,4 @@ public enum CryptoExchange
     [UsedImplicitly]
     [EnumMember(Value = "CBSE")]
     Cbse,
-
-    /// <summary>
-    /// Not supported now.
-    /// </summary>
-    [UsedImplicitly]
-    [Obsolete("This enum member is now obsolete and will be deleted in upcoming major release.", true)]
-    [EnumMember(Value = "GNSS")]
-    Gnss
 }
