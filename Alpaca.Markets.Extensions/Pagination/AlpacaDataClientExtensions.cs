f0bba3f27ac94fd1bb7a392d37907ddb91f04160
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-11-05 18:46:10 +0100

    Issue #567 - Extensions methods added for the new acution-requesting methods.
    (cherry picked from commit 3daf4a560c8b72a59569049c0f355c3cfcf9c52e)

--- Alpaca.Markets.Extensions/Pagination/AlpacaDataClientExtensions.cs
+++ Alpaca.Markets.Extensions/Pagination/AlpacaDataClientExtensions.cs
@@ -143,7 +143,291 @@ public static class AlpacaDataClientExtensions
         getValidatedRequestWithoutPageToken(request.EnsureNotNull())
             .GetResponsesByPages(client.EnsureNotNull().ListNewsArticlesAsync, cancellationToken);
 
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.ListHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of items (in form of <see cref="IAsyncEnumerable{IAuction}"/> interface) so they
+    /// can be consumed by the <c>await foreach</c> statement on the caller side.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IAuction> GetHistoricalAuctionsAsAsyncEnumerable(
+        this IAlpacaDataClient client,
+        HistoricalAuctionsRequest request) =>
+        GetHistoricalAuctionsAsAsyncEnumerable(client, request, CancellationToken.None);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.ListHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of items (in form of <see cref="IAsyncEnumerable{IAuction}"/> interface) so they
+    /// can be consumed by the <c>await foreach</c> statement on the caller side.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IAuction> GetHistoricalAuctionsAsAsyncEnumerable(
+        this IAlpacaDataClient client,
+        HistoricalAuctionsRequest request,
+        CancellationToken cancellationToken) =>
+        getValidatedRequestWithoutPageToken(request.EnsureNotNull())
+            .GetResponsesByItems(client.EnsureNotNull().ListHistoricalAuctionsAsync, cancellationToken);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.GetHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of items (in form of dictionary of the <see cref="IAsyncEnumerable{IAuction}"/>
+    /// interface instances) so they  can be consumed by the <c>await foreach</c> statement on the caller side.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IReadOnlyDictionary<String, IAsyncEnumerable<IAuction>>
+        GetHistoricalAuctionsDictionaryOfAsyncEnumerable(
+            this IAlpacaDataClient client,
+            HistoricalAuctionsRequest request) =>
+        GetHistoricalAuctionsDictionaryOfAsyncEnumerable(client, request, CancellationToken.None);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.GetHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of items (in form of dictionary of the <see cref="IAsyncEnumerable{IAuction}"/>
+    /// interface instances) so they  can be consumed by the <c>await foreach</c> statement on the caller side.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IReadOnlyDictionary<String, IAsyncEnumerable<IAuction>>
+        GetHistoricalAuctionsDictionaryOfAsyncEnumerable(
+            this IAlpacaDataClient client,
+            HistoricalAuctionsRequest request,
+            CancellationToken cancellationToken) =>
+        getValidatedRequestWithoutPageToken(request.EnsureNotNull())
+            .GetResponsesByItems(client.EnsureNotNull().GetHistoricalAuctionsAsync, cancellationToken);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.ListHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of response pages with items so they can be consumed by the <c>await foreach</c>
+    /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IReadOnlyList<IAuction>> GetHistoricalAuctionsPagesAsAsyncEnumerable(
+        this IAlpacaDataClient client,
+        HistoricalAuctionsRequest request) =>
+        GetHistoricalAuctionsPagesAsAsyncEnumerable(client, request, CancellationToken.None);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.ListHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of response pages with items so they can be consumed by the <c>await foreach</c>
+    /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IReadOnlyList<IAuction>> GetHistoricalAuctionsPagesAsAsyncEnumerable(
+        this IAlpacaDataClient client,
+        HistoricalAuctionsRequest request,
+        CancellationToken cancellationToken) =>
+        getValidatedRequestWithoutPageToken(request.EnsureNotNull())
+            .GetResponsesByPages(client.EnsureNotNull().ListHistoricalAuctionsAsync, cancellationToken);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.GetHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of response pages with items so they can be consumed by the <c>await foreach</c>
+    /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IReadOnlyDictionary<String, IReadOnlyList<IAuction>>>
+        GetHistoricalAuctionsMultiPagesAsAsyncEnumerable(
+            this IAlpacaDataClient client,
+            HistoricalAuctionsRequest request) =>
+        GetHistoricalAuctionsMultiPagesAsAsyncEnumerable(client, request, CancellationToken.None);
+
+    /// <summary>
+    /// Gets all items provided by <see cref="IAlpacaDataClient.GetHistoricalAuctionsAsync"/> in pagination
+    /// mode as single stream of response pages with items so they can be consumed by the <c>await foreach</c>
+    /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
+    /// </summary>
+    /// <param name="client">Target instance of the <see cref="IAlpacaDataClient"/> interface.</param>
+    /// <param name="request">Original historical auctions request (with empty next page token).</param>
+    /// <param name="cancellationToken">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
+    /// </exception>
+    [UsedImplicitly]
+    [CLSCompliant(false)]
+    public static IAsyncEnumerable<IReadOnlyDictionary<String, IReadOnlyList<IAuction>>>
+        GetHistoricalAuctionsMultiPagesAsAsyncEnumerable(
+            this IAlpacaDataClient client,
+            HistoricalAuctionsRequest request,
+            CancellationToken cancellationToken) =>
+        getValidatedRequestWithoutPageToken(request.EnsureNotNull())
+            .GetResponsesByPages(client.EnsureNotNull().GetHistoricalAuctionsAsync, cancellationToken);
+
     private static NewsArticlesRequest getValidatedRequestWithoutPageToken(
         IHistoricalRequest<NewsArticlesRequest, INewsArticle> request) =>
+        request.GetValidatedRequestWithoutPageToken();
+
+    private static HistoricalAuctionsRequest getValidatedRequestWithoutPageToken(
+        IHistoricalRequest<HistoricalAuctionsRequest, IAuction> request) =>
         request.GetValidatedRequestWithoutPageToken();
 }
