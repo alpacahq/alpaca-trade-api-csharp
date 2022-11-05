3daf4a560c8b72a59569049c0f355c3cfcf9c52e
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2022-11-05 18:46:10 +0100

    Issue #567 - Extensions methods added for the new acution-requesting methods.

--- Alpaca.Markets.Extensions/Pagination/HistoricalBarsClientExtensions.cs
+++ Alpaca.Markets.Extensions/Pagination/HistoricalBarsClientExtensions.cs
@@ -11,7 +11,7 @@ public static partial class HistoricalBarsClientExtensions
     /// can be consumed by the <c>await foreach</c> statement on the caller side.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <exception cref="RequestValidationException">
     /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
     /// </exception>
@@ -44,7 +44,7 @@ public static partial class HistoricalBarsClientExtensions
     /// can be consumed by the <c>await foreach</c> statement on the caller side.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
@@ -82,7 +82,7 @@ public static partial class HistoricalBarsClientExtensions
     /// interface instances) so they  can be consumed by the <c>await foreach</c> statement on the caller side.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <exception cref="RequestValidationException">
     /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
     /// </exception>
@@ -116,7 +116,7 @@ public static partial class HistoricalBarsClientExtensions
     /// interface instances) so they  can be consumed by the <c>await foreach</c> statement on the caller side.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
@@ -155,7 +155,7 @@ public static partial class HistoricalBarsClientExtensions
     /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <exception cref="RequestValidationException">
     /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
     /// </exception>
@@ -188,10 +188,25 @@ public static partial class HistoricalBarsClientExtensions
     /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
+    /// <exception cref="RequestValidationException">
+    /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
+    /// </exception>
+    /// <exception cref="HttpRequestException">
+    /// The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.
+    /// </exception>
+    /// <exception cref="RestClientErrorException">
+    /// The response contains an error message or the received response cannot be deserialized properly due to JSON schema mismatch.
+    /// </exception>
+    /// <exception cref="SocketException">
+    /// The initial TPC socket connection failed due to an underlying low-level network connectivity issue.
+    /// </exception>
+    /// <exception cref="TaskCanceledException">
+    /// .NET Core and .NET 5 and later only: The request failed due to timeout.
+    /// </exception>
     /// <exception cref="ArgumentNullException">
     /// The <paramref name="client"/> or <paramref name="request"/> argument is <c>null</c>.
     /// </exception>
@@ -211,7 +226,7 @@ public static partial class HistoricalBarsClientExtensions
     /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <exception cref="RequestValidationException">
     /// The <paramref name="request"/> argument contains invalid data or some required data is missing, unable to create a valid HTTP request.
     /// </exception>
@@ -245,7 +260,7 @@ public static partial class HistoricalBarsClientExtensions
     /// statement on the caller side as sequence of 'batches' instead of sequence of items itself.
     /// </summary>
     /// <param name="client">Target instance of the <see cref="IHistoricalBarsClient{TRequest}"/> interface.</param>
-    /// <param name="request">Original historical minute bars request (with empty next page token).</param>
+    /// <param name="request">Original historical bars request (with empty next page token).</param>
     /// <param name="cancellationToken">
     /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
     /// </param>
