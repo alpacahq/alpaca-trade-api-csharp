8d7df67611a5c3c8719dc513e429e35fac9a3aff
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2023-11-07 09:55:36 +0100

    Added the new `Unknown` enumeration member into the `AssetAttributes` enumeration as fallback option.

---
+++ Alpaca.Markets/Helpers/AssetAttributesEnumConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Alpaca.Markets;
+
+[SuppressMessage(
+    "Microsoft.Performance", "CA1812:Avoid uninstantiated internal classes",
+    Justification = "Object instances of this class will be created by Newtonsoft.JSON library.")]
+internal sealed class AssetAttributesEnumConverter : StringEnumConverter
+{
+    public override Object? ReadJson(
+        JsonReader reader,
+        Type objectType,
+        Object? existingValue,
+        JsonSerializer serializer)
+    {
+        try
+        {
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+        catch (JsonSerializationException)
+        {
+            return AssetAttributes.Unknown;
+        }
+    }
+}
