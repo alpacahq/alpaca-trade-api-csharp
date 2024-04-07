7b21663c468fbe59daa7198e8162e73568e5fb75
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2024-04-07 16:31:07 +0200

    Issue #741 - Added the `PositionIntent` enum and related properties into the `NewOrderRequest` and `OrderBase` classes.

---
+++ Alpaca.Markets/Enums/PositionIntent.cs
@@ -0,0 +1,36 @@
+﻿namespace Alpaca.Markets;
+
+/// <summary>
+/// Position intent for order placement in Alpaca REST API.
+/// </summary>
+[JsonConverter(typeof(StringEnumConverter))]
+public enum PositionIntent
+{
+    /// <summary>
+    /// Buy to open a long position.
+    /// </summary>
+    [UsedImplicitly]
+    [EnumMember(Value = "buy_to_open")]
+    BuyToOpen,
+
+    /// <summary>
+    /// Buy to close a short position.
+    /// </summary>
+    [UsedImplicitly]
+    [EnumMember(Value = "buy_to_close")]
+    BuyToClose,
+
+    /// <summary>
+    /// Sell to open a short position.
+    /// </summary>
+    [UsedImplicitly]
+    [EnumMember(Value = "sell_to_open")]
+    SellToOpen,
+
+    /// <summary>
+    /// Sell to close a long position.
+    /// </summary>
+    [UsedImplicitly]
+    [EnumMember(Value = "sell_to_close")]
+    SellToClose,
+}
