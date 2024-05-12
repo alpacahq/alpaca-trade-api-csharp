1ea8760bb9eaf1d9a1dccc3e3496fc3b959405b7
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2024-05-12 16:29:40 +0200

    Fix minor R# issue (unused method)

--- Alpaca.Markets/Helpers/HttpClientExtensions.Post.cs
+++ Alpaca.Markets/Helpers/HttpClientExtensions.Post.cs
@@ -4,14 +4,6 @@ internal static partial class HttpClientExtensions
 {
     public static Task<Boolean> TryPostAsync(
         this HttpClient httpClient,
-        UriBuilder uriBuilder,
-        RateLimitHandler rateLimitHandler,
-        CancellationToken cancellationToken) =>
-        callAndReturnSuccessCodeAsync(
-            httpClient, HttpMethod.Post, uriBuilder.Uri, rateLimitHandler, cancellationToken);
-
-    public static Task<Boolean> TryPostAsync(
-        this HttpClient httpClient,
         String endpointUri,
         RateLimitHandler rateLimitHandler,
         CancellationToken cancellationToken) =>
