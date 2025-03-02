9d5599f033b931792fb841b77b72593b3687994c
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2025-03-02 17:08:04 +0100

    Issue #787 - Basic authentication was added to connect Broker API.

---
+++ Alpaca.Markets/Authentication/BasicKey.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Net;
+
+namespace Alpaca.Markets;
+
+/// <summary>
+/// Basie key for Alpaca Broker API authentication.
+/// </summary>
+[UsedImplicitly]
+public sealed class BasicKey : SecurityKey
+{
+    /// <summary>
+    /// Creates new instance of <see cref="BasicKey"/> object.
+    /// </summary>
+    /// <param name="credential">User credentials (name and password).</param>
+    /// <exception cref="ArgumentNullException">
+    /// The <paramref name="credential"/> argument is <c>null</c>.
+    /// </exception>
+    public BasicKey(
+        NetworkCredential credential)
+        : base(Convert.ToBase64String(Encoding.ASCII.GetBytes(
+            $"{credential.EnsureNotNull().UserName}:{credential.Password}")))
+    {
+    }
+
+    internal override IEnumerable<KeyValuePair<String, String>> GetAuthenticationHeaders()
+    {
+        yield return new KeyValuePair<String, String>(
+            "Authorization", "Basic " + Value);
+    }
+
+    internal override JsonAuthRequest.JsonData GetAuthenticationData() =>
+        throw new InvalidOperationException();
+
+    [ExcludeFromCodeCoverage]
+    internal override JsonAuthentication GetAuthentication() =>
+        throw new InvalidOperationException();
+}
